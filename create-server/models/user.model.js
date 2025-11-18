01eff5a53544f171ea2fc9e308e2a64f6326b8d5
Devendradhote001 <ddhote780@gmail.com>
2025-11-18 12:44:26 +0530

    dsds

--- create-server/models/user.model.js
+++ create-server/models/user.model.js
@@ -1,14 +1,27 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  mobile: Number,
-  email: String,
-  password: {
+  name: {
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 10,
+    maxlength: 10,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
 });
 
-const userModel = mongoose.model("users", userSchema);
-module.exports = userModel;
+const UserModel = mongoose.model("user", userSchema);
+module.exports = UserModel;
--- create-server/server.js
+++ create-server/server.js
@@ -1,44 +1,45 @@
 const express = require("express");
-const userModel = require("./models/user.model");
-const mongoose = require("mongoose");
+const { default: mongoose } = require("mongoose");
+const UserModel = require("./models/user.model");
 
 const app = express();
 
-let connectDb = async () => {
+app.use(express.json());
+
+const connectDB = async () => {
   try {
-    let res = await mongoose.connect("mongodb://0.0.0.0/doremon");
+    let res = await mongoose.connect("mongodb://0.0.0.0/raftaar");
     if (res) {
       console.log("mongodb connected");
     }
   } catch (error) {
-    console.log("error in mongodb");
+    console.log("error while connecting md");
   }
 };
 
-connectDb();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("heyy i m express");
 });
 
-app.get("/create-user", async (req, res) => {
-  let newUser = await userModel.create({
-    username: "rajurastogi",
-    mobile: 123456789,
-    email: "raju@gmail.com",
-  });
+app.post("/create-user", async (req, res) => {
+  try {
+    let { name, email, mobile, password } = req.body;
 
-  console.log(newUser);
+    let user = await UserModel.create({
+      name,
+      email,
+      mobile,
+      password,
+    });
 
-  res.send(newUser);
+    return res.send(user);
+  } catch (error) {
+    return res.send(error);
+  }
 });
 
 app.listen(3000, () => {
   console.log("i m running on port 3000");
 });
-
-// install homebrew
-// brew tap mongodb/brew
-// brew update
-// brew install mongodb-community@8.1
-// brew services start mongodb-community@8.1
