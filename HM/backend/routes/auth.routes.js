44eb6ba073feac71280f7b3fdcdafe56eb352f3b
Devendradhote001 <ddhote780@gmail.com>
2025-12-05 18:38:21 +0530

    dxfdsfe

--- HM/backend/routes/auth.routes.js
+++ HM/backend/routes/auth.routes.js
@@ -1,6 +1,12 @@
 import express from "express";
-import { registerController } from "../controllers/auth.controllers.js";
+import {
+  loginController,
+  logoutController,
+  registerController,
+} from "../controllers/auth.controllers.js";
 
 export const router = express.Router();
 
 router.post("/register", registerController);
+router.post("/login", loginController);
+router.post("/logout", logoutController);
